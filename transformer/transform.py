43c0dea312dca2465ea2af175191789a0a211a6a
Chibu <chibuezeanalyst@gmail.com>
2025-11-06 12:40:39 +0100

    Modify transform.py

--- transformer/transform.py
+++ transformer/transform.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # load existing data 
-df = pd.read_parquet("https://huggingface.co/datasets/electricsheepafrica/nigerian_energy_and_utilities_billing_payments/resolve/main/nigerian_energy_and_utilities_billing_payments.parquet")
+df = pd.read_csv("https://huggingface.co/datasets/electricsheepafrica/nigerian_energy_and_utilities_billing_payments/resolve/main/nigerian_energy_and_utilities_billing_payments.csv")
 
 # Split the billing_month column into month and year
 df[['year', 'month']] = df['billing_month'].str.split('-', expand=True)
@@ -10,4 +10,4 @@ df[['year', 'month']] = df['billing_month'].str.split('-', expand=True)
 cols = ['customer_id', 'disco', 'billing_month', 'year', 'month', 'tariff_band', 'kwh', 'price_ngn_kwh', 'amount_billed_ngn', 'amount_paid_ngn', 'paid_on_time', 'arrears_ngn']
 df = df[cols]
 
-print(df.head())
+print(df.head(10))
